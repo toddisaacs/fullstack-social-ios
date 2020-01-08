6b700c1b2c106a0796e865194254b83b6ff38ab0
Todd Isaacs <teisaacs21w@gmail.com>
2020-01-08 09:35:07 -0500

    Initial Commit

---
+++ fullstack-social-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fullstack-social-ios
+//
+//  Created by Todd Isaacs on 1/8/20.
+//  Copyright © 2020 Todd Isaacs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
